c1759af79cea578ad4da2267aabf0be2f391c173
babub22 <vladlesovoy2@gmail.com>
2023-05-14 22:02:03 +0200

    client: Add new comp `PostButton`

---
+++ client/src/components/navigationBar/PostButton.js
@@ -0,0 +1,7 @@
+export function PostButton() {
+  return (
+    <button className="pr-2 py-2 duration-200 rounded-full bg-sky-500/100 hover:bg-sky-600/80 min-w-full mt-6 mx-2 text-white">
+      Post
+    </button>
+  );
+}
